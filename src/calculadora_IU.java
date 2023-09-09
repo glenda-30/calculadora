9acc593db02fa2d4c044a7dfd9bfb2c89c3c21f8
glenda <glenda.30.0575@gmail.com>
2023-09-08 19:30:35 -0500

    agregue codigo al boton sumar

--- src/calculadora_IU.java
+++ src/calculadora_IU.java
@@ -1,3 +1,6 @@
+
+import javax.swing.JOptionPane;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -26,21 +29,117 @@ public class calculadora_IU extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        jLabel1 = new javax.swing.JLabel();
+        txtnor1 = new javax.swing.JTextField();
+        jLabel2 = new javax.swing.JLabel();
+        txtnor2 = new javax.swing.JTextField();
+        btnsumar = new javax.swing.JButton();
+        btnsumar1 = new javax.swing.JButton();
+        btnsumar2 = new javax.swing.JButton();
+        btnsumar3 = new javax.swing.JButton();
+
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        jLabel1.setText("nro1");
+
+        jLabel2.setText("nro2");
+
+        btnsumar.setText("sumar");
+        btnsumar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnsumarActionPerformed(evt);
+            }
+        });
+
+        btnsumar1.setText("restar");
+        btnsumar1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnsumar1ActionPerformed(evt);
+            }
+        });
+
+        btnsumar2.setText("dividir");
+        btnsumar2.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnsumar2ActionPerformed(evt);
+            }
+        });
+
+        btnsumar3.setText("multiplicar");
+        btnsumar3.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnsumar3ActionPerformed(evt);
+            }
+        });
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addGap(37, 37, 37)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING, false)
+                    .addGroup(javax.swing.GroupLayout.Alignment.LEADING, layout.createSequentialGroup()
+                        .addComponent(btnsumar)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                        .addComponent(btnsumar1)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 24, Short.MAX_VALUE)
+                        .addComponent(btnsumar2)
+                        .addGap(18, 18, 18)
+                        .addComponent(btnsumar3))
+                    .addGroup(javax.swing.GroupLayout.Alignment.LEADING, layout.createSequentialGroup()
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(jLabel1, javax.swing.GroupLayout.Alignment.TRAILING)
+                            .addComponent(jLabel2))
+                        .addGap(44, 44, 44)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                            .addComponent(txtnor2, javax.swing.GroupLayout.DEFAULT_SIZE, 230, Short.MAX_VALUE)
+                            .addComponent(txtnor1))))
+                .addContainerGap(49, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addGap(20, 20, 20)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                    .addComponent(txtnor1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jLabel1))
+                .addGap(35, 35, 35)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabel2)
+                    .addComponent(txtnor2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(47, 47, 47)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(btnsumar)
+                    .addComponent(btnsumar3)
+                    .addComponent(btnsumar2)
+                    .addComponent(btnsumar1))
+                .addContainerGap(135, Short.MAX_VALUE))
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void btnsumarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnsumarActionPerformed
+        // TODO add your handling code here:
+        double nro1 = Double.parseDouble(txtnor1.getText());
+        double nro2 = Double.parseDouble(txtnor2.getText());
+        double sumar=nro1+nro2;
+        JOptionPane.showMessageDialog(this,"la suma es :"+sumar);
+
+    }//GEN-LAST:event_btnsumarActionPerformed
+
+    private void btnsumar1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnsumar1ActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_btnsumar1ActionPerformed
+
+    private void btnsumar2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnsumar2ActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_btnsumar2ActionPerformed
+
+    private void btnsumar3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnsumar3ActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_btnsumar3ActionPerformed
 
     /**
      * @param args the command line arguments
@@ -78,5 +177,13 @@ public class calculadora_IU extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton btnsumar;
+    private javax.swing.JButton btnsumar1;
+    private javax.swing.JButton btnsumar2;
+    private javax.swing.JButton btnsumar3;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JTextField txtnor1;
+    private javax.swing.JTextField txtnor2;
     // End of variables declaration//GEN-END:variables
 }
